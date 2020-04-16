6dc754a2f809e4e8f87236581769b76b22376964
StaffevDA <dimstf@gmail.com>
2020-04-16 15:00:50 +0700

    Change #4 in irrverbs.c - start creating a point system

--- irrverbs.c
+++ irrverbs.c
@@ -9,15 +9,24 @@ ini irrverbs()
         printf("Cannot open file IV.txt");
         return -1;
     }
-    int mas[200],i,k,n=40;
-    IrrVerb correct,user;
+    int score=0,mas[200],j,i,k,n=40;
+    IrrVerb correct;
     for(i=0;i<n;i++)
     {
+        int point=0;
         char str1[13],str2[13];
         k=randverb(&mas);
         read_verb(&correct);
-        printf("%s",user->correct[0]);
+        printf("%s",correct->verb[0]);
         scanf("%s%s",&str1,&str2);
+        point++;
+        for(i=0;i<13;i++)
+        {
+            if(str1[i]!=correct->verb[1][i])
+            {
+                point--;
+            }
+        }
     }
 
 
