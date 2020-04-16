6c0d7cb2f33d31e648963ef5f0a50402292bb870
StaffevDA <dimstf@gmail.com>
2020-04-16 13:45:24 +0700

    Change #2 in irrverbs.c - First lines of the function irrverbs

--- irrverbs.c
+++ irrverbs.c
@@ -9,5 +9,15 @@ ini irrverbs()
         printf("Cannot open file IV.txt");
         return -1;
     }
-    
-}
+    int mas[200],i,k,n=40;
+    IrrVerb correct,user;
+    for(i=0;i<n;i++)
+    {
+        k=randverb(&mas);
+        read_verb(&correct);
+		user->verb[0]=correct->verb[0];
+        
+    }
+
+
+}    
