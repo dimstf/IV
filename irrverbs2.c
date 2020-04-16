f47bd56e861e9a916cf2cfb8289eb2f79fbb5d53
StaffevDA <dimstf@gmail.com>
2020-04-16 15:26:26 +0700

    Change #2 in irrverbs2.c - first version of function check_answer

--- irrverbs2.c
+++ irrverbs2.c
@@ -1,5 +1,16 @@
 int check_answer(IrrVerb *corr,char *s1,char *s2)
 {
-    int p;
-	
+    int p=1;i;
+    for(i=0;i<13;i++)
+    {
+        if(corr->verb[1][i]!=s1[i])
+        {
+            p=0;
+        }
+        if(corr->verb[2][i]!=s2[i])
+        {
+            p=0;
+        }
+    }
+    return p;
 }
