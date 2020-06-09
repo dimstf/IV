0950b6ff4e954cd6b6ee22413fa10e62e3101cbe
StaffevDA <dimstf@gmail.com>
2020-06-09 16:58:02 +0700

    Testing and fixing some bugs

--- test/i2.h
+++ test/i2.h
@@ -1,10 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
 #if !defined irrverbs_h
 #define irrverbs_h
 typedef struct {
     char verb[3][14];
 } IrrVerb;
+extern int check_answer(IrrVerb *corr,char *s1,char *s2);
 #endif
